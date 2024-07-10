144c1a8aaa0187da102d61e955621ff63d5f1f08
Sittitep Tosuwan <sittitep.tosuwan@tempsquad.co>
2024-07-10 21:52:08 +0700

    feat: update books index to return a list of books

--- app/api/books.rb
+++ app/api/books.rb
@@ -2,10 +2,33 @@ class Books < Grape::API
   format :json
   prefix :api
 
+  helpers do
+    def serialize_books(books)
+      books.map do |book|
+        serialize_book(book)
+      end
+    end
+  
+    def serialize_book(book)
+      {
+        id: book.id,
+        title: book.title,
+        author: book.author,
+        genre: book.genre,
+        year: book.year
+      }
+    end
+  end
+
   resource :books do
     desc 'Return a list of books'
     get do
-      status 200
+      books = Book.all
+
+      {
+        data: serialize_books(books),
+        error: nil
+      }
     end
   end
 end
